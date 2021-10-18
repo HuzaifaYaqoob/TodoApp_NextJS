f39e01af5237ddaab57f6a7471256620f444217a
ZeevhY <zeevhyorg@gmail.com>
2021-10-18 18:10:15 +0500

    18 Oct 2021 Todo App Front End Complete Except Add Task Page

--- Components/Form/Form.js
+++ Components/Form/Form.js
@@ -8,11 +8,11 @@ const FormLabel = (props) => {
     )
 }
 
-export const InputField = ({ className, ...props }) => {
+export const InputField = ({ className, Label, ...props }) => {
     return (
         <div className='w-full mb-2'>
             <FormLabel>
-                {props.Label}
+                {Label}
             </FormLabel>
             <input className={`w-full outline-none focus:ring-2 focus:ring-purple-500 ring-offset-1 rounded py-1 px-5 ${className}`} {...props} />
         </div>
@@ -30,63 +30,35 @@ export const TextArea = ({ className, Label, ...props }) => {
     )
 }
 
-const DropDownItem = ({data , setActiveItem , setDropDownActive}) => {
-    
-    const HandleActiveItem = () =>{
-        setActiveItem(data)
-        setDropDownActive(false)
-    }
+export const DropDownItem = ({className , data , activeItem_state , ...props}) => {
+    const {active_item , setActive_Item} = activeItem_state
     
     return (
-        <p onClick={()=>{HandleActiveItem()}} className={`bg-gray-300 rounded mb-1 py-1 px-2 hover:bg-${data.bg_color}-500 transition-all cursor-pointer hover:text-white`}>{data.text}</p>
+        <p onClick={()=>{setActive_Item(data)}} className={`bg-gray-300 rounded mb-1 py-1 px-2 transition-all cursor-pointer hover:text-white ${className}`}>{data.text}</p>
     )
 }
 
-export const DropDown = () => {
+export const DropDown = ({activeItem,...props}) => {
     const [dropDownActive, setDropDownActive] = useState(false)
-    const [activeItem , setActiveItem] = useState(
-        {
-            text: 'To Do',
-            bg_color: 'red',
-        }
-    )
-    const dropDownItems = [
-        {
-            text: 'To Do',
-            bg_color: 'red',
-        },
-        {
-            text: 'Doing',
-            bg_color: 'yellow',
-        },
-        {
-            text: 'In Progress',
-            bg_color: 'gray',
-        },
-        {
-            text: 'Done',
-            bg_color: 'purple',
-        },
-        {
-            text: 'Completed',
-            bg_color: 'green',
-        },
-
-    ]
+    
     return (
         <div className='relative w-full flex-1'>
-            <div className={`bg-${activeItem.bg_color}-500 py-1 px-2 text-white rounded w-full cursor-pointer`} onClick={() => { setDropDownActive(!dropDownActive) }}>
+            <div className={`py-1 px-2 text-white rounded w-full cursor-pointer ` + (activeItem.bg_color ? `bg-${activeItem.bg_color}-500` : 'bg-gray-500')} onClick={() => { setDropDownActive(!dropDownActive) }}>
                 <p>{activeItem.text}</p>
             </div>
             <div className={'absolute top-full left-0 right-0 transform translate-y-1 rounded shadow-lg bg-white py-1 px-2 ' + (dropDownActive ? 'block' : 'hidden')}>
-                {
-                    dropDownItems.map((item, ind) => {
-                        return (
-                            <DropDownItem data = {item} setActiveItem={setActiveItem} setDropDownActive={setDropDownActive} />
-                        )
-                    })
-                }
+                {props.children}
             </div>
+        </div>
+    )
+}
+
+
+export const DateTime = () =>{
+    return(
+        <div>
+            <FormLabel>Ends Date</FormLabel>
+            <input type='date'  />
         </div>
     )
 }
