87c212b58a5103792e74e7b5b270ce26f2e9dc82
ZeevhY <zeevhyorg@gmail.com>
2021-10-25 09:37:29 +0500

    Final Commit

--- Components/Task/Todo.js
+++ Components/Task/Todo.js
@@ -33,10 +33,10 @@ const Todo = ({ className, data, ...props }) => {
         <div className={`group flex items-center justify-between gap-3 bg-white mb-4 hover:shadow-md transition-all cursor-pointer rounded-md p-3 border-l-4 ${className}`}>
             <span className='w-10 h-10 rounded-full bg-gray-300 bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url('${apiBaseURL + data.image}')` }}></span>
             <div className='flex-1'>
-                <h3 className='flex-1 group-hover:text-gray-900 text-gray-600 font-semibold' onClick={() => { router.push(`/task/${data.id}/`) }}>
+                <h3 className='flex-1 mb-1 group-hover:text-gray-900 text-gray-600 font-semibold' onClick={() => { router.push(`/task/${data.id}/`) }}>
                     {data.title}
                 </h3>
-                <p className='bg-gray-200 py-1 px-3 rounded inline-block'>{data.status}</p>
+                <p className='bg-gray-200 text-sm py-1 px-3 rounded inline-block'>{data.status}</p>
             </div>
             <span className='group-hover:opacity-100 opacity-0 bg-red-100 py-1 px-2 rounded hover:bg-red-300' onClick={() => { DeleteTodo() }}>Delete</span>
         </div>
